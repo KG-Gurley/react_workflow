95196c3139a2c65f0b9d0bddf10ea729a0577518
kgurley <gengxun.k.gurley@gmail.com>
2022-01-10 21:41:21 -0600

    foo: this will fail

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload workflow.
+          Edit <code>src/App.js</code> and save to reload workflow test1.
         </p>
         <a
           className="App-link"
