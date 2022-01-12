3331819421c2cdb776c38aeaa02a8252e4039cb6
kgurley <gengxun.k.gurley@gmail.com>
2022-01-11 20:07:08 -0600

    build: testting

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React56
         </a>
       </header>
     </div>
