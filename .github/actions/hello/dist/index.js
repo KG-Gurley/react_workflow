e742475eebf2f9ec802a4a599c2fbcdea6155cea
kgurley <gengxun.k.gurley@gmail.com>
2022-01-12 09:50:06 -0600

    feat: add github toolkit core functions

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -8445,14 +8445,21 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 try {
-  throw new Error('error message');
+  // throw new Error('error message');
+  core.debug('Debug message');
+  core.warning('Warning message');
+  core.error('Error message');
   const name = core.getInput('who-to-greet');
+  core.setSecret(name);
   console.log(`Hello ${name}`);
 
   const time = new Date();
   core.setOutput('time', time.toTimeString());
 
+  core.startGroup('Logging github object');
   console.log(JSON.stringify(github, null, '\t'));
+  core.endGroup();
+  core.exportVariable('HELLO', 'hello');
 } catch (error) {
   core.setFailed(error.message);
 }
